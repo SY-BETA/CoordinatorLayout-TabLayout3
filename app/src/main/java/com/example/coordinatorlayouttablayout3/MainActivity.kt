85e755ec7ed8f8fc7d85c62eae087904afa3328f
prgsy <prgsy001sub@gmail.com>
2020-03-01 12:11:03 +0900

    initial commit

---
+++ app/src/main/java/com/example/coordinatorlayouttablayout3/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.coordinatorlayouttablayout3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.coordinatorlayouttablayout2.ViewPagerAdapter
+import com.example.coordinatorlayouttablayout2.ViewPagerFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        val fragmentList = arrayListOf<Fragment>()
+        for(i in 0..3){
+            fragmentList.add(ViewPagerFragment(i))
+        }
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, fragmentList)
+        viewPager.adapter = adapter
+
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
